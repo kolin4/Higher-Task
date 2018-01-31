836184bce37d5c307b14a20ba93d5d2d7314977b
Krzysztof Kolacz <kolin4@gmail.com>
2018-01-31 18:55:05 +0100

    added prev and next btn

--- src/components/main/images.js
+++ src/components/main/images.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import SingleImage from './singleImage';
+
 
 class Images extends React.Component {
     constructor(props){
@@ -20,7 +22,7 @@ class Images extends React.Component {
                     let id = idArray[idArray.length-1];
                     arrayId.push(`http://source.unsplash.com/${id}`);
                 }
-           
+                arrayId.shift();
                 this.setState({
                     data:arrayId,
                     isData:true
@@ -32,18 +34,51 @@ class Images extends React.Component {
                 
             })
     }
-   
+    changePhoto =(event)=>{
+        let elemId = event.target.dataset.id;
+        let page = this.state.page;
+        switch(elemId){
+            case 'prev' :
+                if(page < 3){
+                    page = 0;
+                } else {
+                    page -= 3
+                }
+                 break;
+            case 'next' :
+                if (page + 3 > this.state.data.length ){
+                    page = this.state.data.length -3;
+                } else {
+                    page += 3;
+                }
+                 break;
+            default :
+                 break;
+        }
+        
+        this.setState({
+            page
+        })
+    }
     render (){
       
-        let imageToLoad = this.state.data.splice(this.state.page,this.state.page+3);
+        let imageToLoad = this.state.data.slice(this.state.page,this.state.page+3);
         console.log(imageToLoad);
         
         return (
             <div className='content'>
                 <div className='main-container'>
-                    {/* {!this.state.isData ? null : (
-
-                    )} */}
+                    <div className='navigation'>
+                        <p><strong onClick={this.changePhoto}  data-id='prev'>prev</strong> / <strong data-id='next' onClick={this.changePhoto} >next</strong></p>
+                    </div>
+                    <div className='images-container' >
+                    {!this.state.isData ? null : (
+                        imageToLoad.map( (elem,index)=>{
+                            return  <SingleImage  key={elem} imgUrl={elem}/>
+                        } )
+                    )}
+                    </div>
+                   
                 </div>
             </div>
         )
--- src/components/main/singleImage.js
+++ src/components/main/singleImage.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+class SingleImage extends React.Component{
+    render(){
+        return (
+            <div className='image'>
+                <img src={this.props.imgUrl} alt='img'/>
+            </div>
+        )
+    }
+}
+
+
+export default SingleImage;
